8c4a48283a428e4b12b177799c35033b35a5b770
Elie Teyssedou <elie@deliver.ee>
2019-04-09 14:26:38 +0200

    Fix Host for socket connections

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :pong, PongWeb.Endpoint,
   http: [port: System.get_env("PORT")],
-  url: [scheme: "https", host: "phoenix-socket-pong", port: 443],
+  url: [scheme: "https", host: "phoenix-socket-pong.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
