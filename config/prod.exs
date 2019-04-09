14fed78d147bd8dbf41ccbab4ccb4960bca13c35
Elie Teyssedou <elie@deliver.ee>
2019-04-09 14:06:13 +0200

    Fix heroku port binding

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :pong, PongWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
+  http: [port: System.get_env("PORT")],
   url: [scheme: "https", host: "phoenix-socket-pong", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
