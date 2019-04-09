e591d1a47c09479b07203382e96d548fa2e909f7
Elie Teyssedou <elie@deliver.ee>
2019-04-09 13:07:38 +0200

    Making the project production ready

--- lib/pong_web/endpoint.ex
+++ lib/pong_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule PongWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :pong
 
   socket "/socket", PongWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
