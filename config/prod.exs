2730d55d7b6222989db896ac9369ccc4585a83b6
Elie Teyssedou <elie@deliver.ee>
2019-04-09 14:22:15 +0200

    Fix prod repo configuration

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ config :pong, PongWeb.Endpoint,
 config :logger, level: :info
 
 # Database configurartion
-config :hello, Hello.Repo,
+config :pong, Pong.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
